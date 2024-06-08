fbe1500f476cd648b5ac25308639e00020a8ff30
Haisyam <bersholawat2019@gmailcom>
2024-06-09 00:33:57 +0700

    add tag meta

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,6 +7,21 @@ export default function Document() {
         <title>
           Zaenal Farm | Jual Domba Aqiqah, Qurban, Ternak, dan Lain-lain.
         </title>
+        <meta
+          name="description"
+          content="Jual Domba Aqiqah, Qurban, Ternak, dan Lain-lain."
+        />
+        <meta name="keywords" content="zaenalfarm, domba, wanasalam" />
+        <meta
+          property="og:title"
+          content="Zaenal Farm | Jual Domba Aqiqah, Qurban, Ternak, dan Lain-lain."
+        />
+        <meta
+          property="og:description"
+          content="Jual Domba Aqiqah, Qurban, Ternak, dan Lain-lain."
+        />
+        <meta property="og:image" content="/public/kurban-2023.jpg" />
+        <link rel="icon" href="/public/kurban-2023.jpg" />
       </Head>
       <body>
         <Main />
