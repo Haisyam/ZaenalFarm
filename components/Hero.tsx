936e38b4009b885285cb40a47f1840261d42e90c
Haisyam <bersholawat2019@gmailcom>
2024-06-09 00:20:24 +0700

    fix

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -75,7 +75,7 @@ export default function Hero() {
           </p>
           <div className="mt-10 flex items-center gap-x-6">
             <a
-              href="https://linktr.ee/qurban35"
+              href="https://wa.me/6283815324321"
               className="rounded-md bg-choco px-3.5 py-1.5 text-base font-semibold leading-7 text-white shadow-sm hover:bg-slate-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
             >
               Pesan sekarang
